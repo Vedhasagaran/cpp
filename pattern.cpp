d5221395df2b1a41a1805a918a87b5a248a00597
Vedhasagaran <sayhaitovedha@gmail.com>
2025-09-23 01:33:47 +0530

    Initial C++ project setup
    - Add pattern.cpp with competitive programming template
    
    - Configure VS Code tasks for compilation and execution
    
    - Set up C++ IntelliSense with MinGW paths
    
    - Add input.txt for testing
    
    - Configure .gitignore for executables and temp files

---
+++ pattern.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+void print(int n) {
+    cout << n;
+}
+
+int main() {
+	int t;
+	cin >> t;
+
+	for(int i = 0; i < t; i++){
+	    int n;
+	    cin >> n;
+	    print(n);
+	    cout << endl;
+	}
+
+	return 0;
+}
