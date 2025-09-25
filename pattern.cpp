6d363f10a74a77abb1f8034a6b4d60aef985b37b
Vedhasagaran <sayhaitovedha@gmail.com>
2025-09-25 23:33:19 +0530

    4-8 solved

--- pattern.cpp
+++ pattern.cpp
@@ -40,7 +40,69 @@ void floydTriangle(int n){
 	}
 }
 
+void print4(int n){
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= i; j++){
+			cout << i << " ";
+		}
+		cout << endl;
+	}
+}
 
+void print5(int n){
+	for(int i = 1; i <= n ; i++){
+		for(int j = 1 ; j <= n - i + 1  ; j++)
+		{
+			cout << "*" << " ";
+		}
+		cout << endl;
+	}
+}
+
+void print6(int n){
+	for(int i = n; i > 0; i--){
+		for(int j=1; j<=i; j++){
+			cout << j << " ";
+		}
+		cout << endl;
+	}
+}
+
+void print7(int n){
+	for(int i = 0; i < n; i++){
+			for(int j = 0; j < n-i; j++){
+			cout << " ";
+		}
+
+		for(int j = 0; j < 2*i+1; j++){
+			cout << "*";
+		}
+
+		for(int j = 0; j < n-i; j++){
+			cout << " ";
+		}
+
+		cout << endl;
+	}
+}
+
+void print8(int n){
+	for(int i = 1; i <= n; i++){
+		for(int j =1; j <i; j++){
+			cout << " ";
+		}
+
+		for(int j = 1; j <= 2*n-(2*i-1); j++){
+			cout << "*";
+		}
+
+		for(int j = 1; j <i; j++){
+			cout << " ";
+		}
+
+		cout << endl;
+	}
+}
 
 int main() {
 	int t;
@@ -49,7 +111,7 @@ int main() {
 	for(int i = 0; i < t; i++){
 	    int n;
 	    cin >> n;
-	    floydTriangle(n);
+	    print8(n);
 	    cout << endl;
 	}
 
