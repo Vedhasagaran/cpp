74f2eb7f2607f90e77461dbc17961ad6f99e8975
Vedhasagaran <sayhaitovedha@gmail.com>
2025-09-27 11:39:22 +0530

    optimised pattern 10

--- pattern.cpp
+++ pattern.cpp
@@ -138,14 +138,11 @@ void print9(int n){
 
 void print10(int n){
 	for(int i = 1 ; i <= 2*n-1 ; i++){
-		if(i <= n){
-			for(int j = 1; j <= i; j++){
-				cout << "*";
-			}
-			cout << endl;
-			continue;
+		int stars = i;
+		if(i > n){
+			stars = 2*n-i;
 		}
-		for(int j = 1; j <= 2*n-i ; j++){
+		for(int j = 1; j <= stars ; j++){
 			cout << "*";
 		}
 		cout << endl;
