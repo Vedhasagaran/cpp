a7f593ba37b195b26074935b2d2b94f8da3fdff6
Vedhasagaran <sayhaitovedha@gmail.com>
2025-09-27 11:56:56 +0530

    11

--- pattern.cpp
+++ pattern.cpp
@@ -12,7 +12,7 @@ void print1(int n) {
 }
 
 void print2(int n) {
-	for(int i = 0 ; i <= n ; i++){
+	for(int i = 0 ; i < n ; i++){
 		for(int j = 0 ; j <= i ; j++){
 			cout << "*" << " ";
 		}
@@ -149,6 +149,30 @@ void print10(int n){
 	}
 }
 
+void print11(int n){
+	for(int i = 1 ; i <= n ; i++){
+		for(int j = 1 ; j <= i ; j++){
+			if(i%2==0){
+				if(j%2!=0){
+					cout << "0 ";
+				}
+				else{
+					cout << "1 ";
+				}
+				
+			}
+			else{
+				if(j%2==0){
+					cout << "0 ";
+				}else{
+				cout << "1 ";
+				}
+			}
+		}
+		cout << endl;
+	}
+}
+
 int main() {
 	int t;
 	cin >> t;
@@ -156,7 +180,7 @@ int main() {
 	for(int i = 0; i < t; i++){
 	    int n;
 	    cin >> n;
-	    print10(n);
+	    print11(n);
 	    cout << endl;
 	}
 
