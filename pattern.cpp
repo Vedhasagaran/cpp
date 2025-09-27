940f8777bb1e309d5163c96451ba36d8c6bd677e
Vedhasagaran <sayhaitovedha@gmail.com>
2025-09-27 12:02:31 +0530

    optimised 11

--- pattern.cpp
+++ pattern.cpp
@@ -152,22 +152,7 @@ void print10(int n){
 void print11(int n){
 	for(int i = 1 ; i <= n ; i++){
 		for(int j = 1 ; j <= i ; j++){
-			if(i%2==0){
-				if(j%2!=0){
-					cout << "0 ";
-				}
-				else{
-					cout << "1 ";
-				}
-				
-			}
-			else{
-				if(j%2==0){
-					cout << "0 ";
-				}else{
-				cout << "1 ";
-				}
-			}
+			cout << ((i + j)%2 == 0 ? "1" : "0") << " ";
 		}
 		cout << endl;
 	}
